2815ad29b79ac809f8a93cd19918f3463dfd6840
RHallanger <rjhallanger@students.nic.edu>
2025-08-26 16:43:31 -0700

    Update basicCalculator.py
    Added an exception handler, I think?

--- Custom Scripts/basicCalculator.py
+++ Custom Scripts/basicCalculator.py
@@ -1,17 +1,26 @@
 print("WOOO BASIC CALCULATOR PROGRAM!\nIt can only support 2 numbers and 1 operator.")
 
-user_number1 = input("Number 1: ")
-user_number2 = input("Number 2: ")
+try:
+    user_number1 = int(input("Number 1: "))
+    user_number2 = int(input("Number 2: "))
+
+except:
+    print('Exception found, did you not use a number?')
+    quit()
+    
 user_operator = input("Operator: ")
 
 if user_operator == "*":
-    print(int(user_number1) * int(user_number2))
+    print(user_number1 * user_number2)
 
-if user_operator == "%":
-    print(int(user_number1) % int(user_number2))
+elif user_operator == "/":
+    print(user_number1 / user_number2)
 
-if user_operator == "+":
-    print(int(user_number1) + int(user_number2))
+elif user_operator == "+":
+    print(user_number1 + user_number2)
 
-if user_operator == "-":
-    print(int(user_number1) - int(user_number2))
+elif user_operator == "-":
+    print(user_number1 - user_number2)
+
+else:
+    print('I can't do that.')
