d0e35000dc61c97f21b31c1cd898af453add06ed
RHallanger <rjhallanger@students.nic.edu>
2025-08-29 12:57:03 -0700

    Simplified even more

--- Custom Scripts/ForLoopProgram.py
+++ Custom Scripts/ForLoopProgram.py
@@ -1,7 +1,5 @@
 ###Simple For loop program
-i=0
-for i in range(10):
-    i += 1
+for i in range(1, 11):
     print(i)
 
 quit()
--- Custom Scripts/WhileLoopProgram.py
+++ Custom Scripts/WhileLoopProgram.py
@@ -1,14 +1,7 @@
 ##### Just a simple while loop counter
 count = 0
-while True:
-    if count >= 10:
-        count = 10
-        break
-
-    else:
-        count += 1
-        pass
-
+while count < 10:
+    count += 1
     print(count)
 
 quit()
