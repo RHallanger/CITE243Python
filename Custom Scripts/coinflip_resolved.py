7eb5b9e1ba3f573f552b77157ddf67a7a106a953
RHallanger <rjhallanger@students.nic.edu>
2025-09-08 16:13:21 -0700

    Not sure...

--- Custom Scripts/coinflip_resolved.py
+++ Custom Scripts/coinflip_resolved.py
@@ -1,39 +1,27 @@
 import random
 
 guess = ''
-
-toss = random.randint(0, 1) #assigns heads or tail in a range of 0 to 1...
-
-match toss: #Converts the numeric value into string heads or tails to match.
-    case 0:
-        toss = 'tails'
-    case 1:
-        toss = 'heads'
-
-#Guess 1
 while guess not in ('heads', 'tails'):
-    print('Guess the coin toss! Enter "heads" or "tails":')
+    print('Guess the coin toss! Enter heads or tails:')
     guess = input()
     guess = guess.lower()
 
-if toss == guess:#Pass
-    print('You got it!')
-    quit()
+toss = random.randint(0, 1)  # 0 is tails, 1 is heads
 
-else: #Fail, retry
+if toss == 0:
+    toss = 'tails'
+else:
+    toss = 'heads'
+
+if toss == guess:
+    print('You got it!')
+
+else:
     print('Nope! Guess again!')
-    guess = ''
-
-#Guess 2
-while guess not in ('heads', 'tails'):
-    print('Guess the coin toss! Enter "heads" or "tails":')
     guess = input()
     guess = guess.lower()
 
-if toss == guess: #Pass
-    print('You got it!')
-    quit()
-
-else: #Fail
-    print('Nope. You are really bad at this game.')
-    quit()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
