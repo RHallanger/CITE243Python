86935b811c1fad7b7bbff326f9cf861fddde820f
RHallanger <rjhallanger@nic.edu>
2025-10-22 09:29:59 -0700

    Added timestamps and removed redundant steps

--- Assignments/Ch-15_EmailFetch.py
+++ Assignments/Ch-15_EmailFetch.py
@@ -22,38 +22,34 @@ import time
 # Rename and transfer the downloaded JSON credentials from client_secret_*.json to credentials-sheets.json
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Do not share these credentials !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+emails = []  # Initialize emails list globally
+### This function will be used after the initial fetch and print of emails to gather any new emails added to the sheet.
 def emailRefresh():
+    # Imports the empty email list to prevent repetition of already found emails.
     global emails
-    workbook = ezsheets.Spreadsheet('https://docs.google.com/spreadsheets/d/1vWAR-nN2S99h3_Af6voad7Ux1ynCXmHbmLiAEd_CWsU/edit?resourcekey=&gid=590686694#gid=590686694') #reinitialize the workbook
+    # fetches the overall google sheets workbook
+    workbook = ezsheets.Spreadsheet('https://docs.google.com/spreadsheets/d/1vWAR-nN2S99h3_Af6voad7Ux1ynCXmHbmLiAEd_CWsU/edit?resourcekey=&gid=590686694#gid=590686694') #reinitializes every loop to gather new emails.
+    # Google Sheets is confusing with sheet of sheets.
+    # Sheet refers to the page we have selected at the bottom of the page.
+    # Workbook refers to the entire document with multiple sheets.
+    # ResponseSheet is grabs the whole sheet from the workbook's list of sheets.
     responseSheet = workbook[0]
+    # Grabs column B which contains the emails.
     newEmails = responseSheet.getColumn('B')
+    # Filters out any empty cells and the header 'Email Address'
     newEmails = list(filter(None, newEmails))
     newEmails.remove('Email Address')
+    # Compares new emails to existing emails and appends any new ones to the emails list.
+    # While printing any new emails found.
+    currentTime = time.strftime("%H:%M:%S")
     for i in newEmails:
         if i not in emails:
             emails.append(i)
-            print(f'New email found: {i}')
+            print(f'[{currentTime}] {i}')
     time.sleep(30) # Delay before next refresh to reduce API calls.
 
-
-### assign the Google Sheets workbook we want to workbook
-workbook = ezsheets.Spreadsheet('https://docs.google.com/spreadsheets/d/1vWAR-nN2S99h3_Af6voad7Ux1ynCXmHbmLiAEd_CWsU/edit?resourcekey=&gid=590686694#gid=590686694')
-### This specifies the sheet in the workbook we actually want to look at.
-# Sheets are the little tabs at the bottom of the page that leads to a different table.
-responseSheet = workbook[0]
-### Now we copy everything in the column that we want to a list labeled emails.
-emails = responseSheet.getColumn('B')
-### Now we have a lot of None data types that make up roughly 100 list items.
-# So we are going to filter all None types from our emails list
-emails = list(filter(None, emails))
-# We also don't need the column name, it isn't an email.
-emails.remove('Email Address')
-### Last, we print to the command line what we have.
 currentTime = time.strftime("%a, %d %b %Y %H:%M:%S") #timestamps
 print(f'[{currentTime}] Emails found in responses:')
-for i in emails:
-    print(i)
-
 # Infiinite loop to keep refreshing emails
 while True:
     emailRefresh()
