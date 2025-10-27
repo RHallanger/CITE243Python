005eb2abf41cebd8277204edd34985a7d9e7f296
RHallanger <rjhallanger@students.nic.edu>
2025-10-27 15:07:16 -0700

    Added remove option

--- Assignments/Ch-16_RecipeDatabase.py
+++ Assignments/Ch-16_RecipeDatabase.py
@@ -1,14 +1,14 @@
 """
 ===========================================================
-Program Name: 
+Program Name: Recipe Database Manager
 Author: Robert Hallanger
 Date: 2025-10-27
 Description:
-    This program performs [brief description of functionality].
-    It is designed to [specific purpose or goal].
+    This program is a simple suite of tools.
+    It is designed to make adjustments to an SQLite database.
     
 Usage:
-    Run the script using Python 3.14. Ensure all dependencies
+    Run the script using Python 3.13. Ensure all dependencies
     are installed before execution.
 
 ===========================================================
@@ -17,8 +17,11 @@ Usage:
 import sqlite3
 connect = sqlite3.connect('recipes.db', isolation_level=None)
 connect.execute('CREATE TABLE IF NOT EXISTS recipe (dish TEXT NOT NULL, ingredients TEXT) STRICT')
+### Made a file named recipes.db with a new table named recipe.
 
+### This menu will cycle constantly with all of the functions necessary to add, search/view, and remove recipes.
 def options():
+    # First we will show the user their options and loop them until they give a valid input.
     print('Recipe Database Menu:')
     print('1) List All Recipes')
     print('2) Add New Recipe')
@@ -26,48 +29,60 @@ def options():
     print('4) Search Recipes by Ingredient')
     print('5) Remove Recipe') ### NEED TO ADD THIS
     print('6) Exit')
+
     choice = 0
-    invalidCount = 0
-    while choice not in ['1', '2', '3', '4', '5']:
-        try:
-            choice = input('Select an option (1-5): ')
+    invalidCount = 0 # This counter is to break up the text to maintain readability of the screen.
 
-        except:
-            print('Invalid option, please select an option between 1-5')
-
+    while choice not in ['1', '2', '3', '4', '5', '6']:
+        choice = input('Select an option (1-6): ')
         invalidCount += 1
+
         if invalidCount >= 5:
             print('\n' * 5)
             invalidCount = 0
             options()
 
     print('\n' * 5)
+    # Spaces out the screen for readability of the next function.
+    # This part will take the user's choice and call the appropriate function and afterwards, return to the options menu.
     match choice:
         case '1':
+            # Lists only the names of the dishes for quicker viewing.
             list_recipes()
             print('\n' * 5)
             options()
 
         case '2':
+            # Allows the user to add a dish and the associated ingredients.
             add_recipe()
             print('\n' * 5)
             options()
 
         case '3':
+            # Searches for recipes by dish name.
             search_dishes()
             print('\n' * 5)
             options()
 
         case '4':
+            # Searches for recipes by ingredient.
             search_recipes()
             print('\n' * 5)
             options()
 
         case '5':
+            # Removes a recipe from the database.
+            remove_recipe()
+            print('\n' * 5)
+            options()
+
+        case '6':
+            # Closes the program.
             print('Exiting the program. Goodbye!')
             exit()
 
         case _:
+            # Catches any invalid input that may have bypassed the initial checks.
             print('Error, invalid option, bypassed exception.')
             print('\n' * 5)
             options()
@@ -86,6 +101,16 @@ def add_recipe():
     ingredients = input('Enter the ingredients (comma-separated): ')
     connect.execute('INSERT INTO recipe (dish, ingredients) VALUES (?, ?)', (dish, ingredients))
     print(f'\nRecipe for {dish} added successfully.')
+
+def remove_recipe():
+    dish = input('Enter the name of the dish to remove: ')
+    cursor = connect.execute('SELECT * FROM recipe WHERE dish = ?', (dish,))
+    recipe = cursor.fetchone()
+    if recipe:
+        connect.execute('DELETE FROM recipe WHERE dish = ?', (dish,))
+        print(f'Recipe for {dish} removed successfully.')
+    else:
+        print(f'No recipe found for dish: {dish}')
 
 def search_recipes():
     ingredient = input('Enter an ingredient to search for: ')
@@ -115,4 +140,5 @@ def search_dishes():
     else:
         print(f'No recipes found for dish: {dish_name}')
 
+# Start the program by calling the options function.
 options()
